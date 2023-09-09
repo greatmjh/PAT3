73ad8d74ffa4a159c4376ad636916fe40f503c39
greatmjh <66207287+greatmjh@users.noreply.github.com>
2023-09-09 09:45:38 +0200

    Implemented battery information class

---
+++ src/mel/battchargecontroller/BatteryInfo.java
@@ -0,0 +1,124 @@
+package mel.battchargecontroller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class BatteryInfo {
+    //Does the system support battery percentage reporting
+    private static boolean percentageAvailable = false;
+
+    //Battery percentage
+    private static int percentage = 0;
+
+    //Does the system support battery temperature reporting
+    private static boolean temperatureAvailable = false;
+
+    //Battery temperature in Celsius
+    private static int temperature = 0;
+
+    //Does the system report whether the device is charging
+    private static boolean chargingStateAvailable = false;
+
+    //Is the system charging
+    private static boolean charging = false;
+
+    //Accessors
+    public static boolean isPercentageAvailable() {
+        return percentageAvailable;
+    }
+
+    public static int getPercentage() {
+        return percentage;
+    }
+
+    public static boolean isTemperatureAvailable() {
+        return temperatureAvailable;
+    }
+
+    public static int getTemperature() {
+        return temperature;
+    }
+
+    public static boolean isChargingStateAvailable() {
+        return chargingStateAvailable;
+    }
+
+    public static boolean isCharging() {
+        return charging;
+    }
+
+    //Queries the system for the latest battery info
+    public static void update() {
+        Runtime rt = Runtime.getRuntime();
+        try {
+            //Get the battery percentage
+            String battPercentageCmd = "Get-CimInstance -ClassName Win32_Battery | Select-Object -ExpandProperty EstimatedChargeRemaining"; //Borrowed from https://powershell.one/wmi/root/cimv2/win32_battery
+            //Run the command
+            String stPercentage = execPowershell(battPercentageCmd);
+            //Attempt to process the output
+            try {
+                percentage = Integer.parseInt(stPercentage);
+                //Set percentageAvailable only if this does not throw an exception
+                percentageAvailable = true;
+            } catch (NumberFormatException n) {
+                //The OS produced invalid data, disregard it
+                System.err.println("Error when parsing result for percentage: " + stPercentage);
+                n.printStackTrace();
+                percentageAvailable = false;
+            }
+
+            //Get the battery temperature
+            String battTempCmd = ""; //TODO: implement this PowerShell code
+            //Run the command
+            String stTemperature = execPowershell(battTempCmd);
+            //Attempt to process the output
+            try {
+                temperature = Integer.parseInt(stTemperature);
+                //Set temperatureAvailable only if this does not throw an exception
+                temperatureAvailable = true;
+            } catch (NumberFormatException n) {
+                System.err.println("Error when parsing result for temperature: " + stTemperature);
+                n.printStackTrace();
+                temperatureAvailable = false;
+            }
+
+            //Get the charging state
+            String chgStCmd = "(Get-CimInstance -Namespace \"ROOT\\WMI\" -ClassName \"BatteryStatus\").Charging"; //Borrowed from https://github.com/gwblok/garytown/blob/master/hardware/HP/BatteryInfo.ps1
+            //Run the command
+            String stChargeState = execPowershell(chgStCmd);
+            //Process the output into a boolean
+            if (stChargeState.equalsIgnoreCase("True")) {
+                chargingStateAvailable = true;
+                charging = true;
+            } else if (stChargeState.equalsIgnoreCase("False")) {
+                chargingStateAvailable = true;
+                charging = false;
+            } else {
+                //The system produced invalid information
+                chargingStateAvailable = false;
+                System.err.println("System returned invalid response for charging state request: " + stChargeState);
+            }
+
+        } catch (IOException ex) {
+            //An error occurred when running commands
+            ex.printStackTrace();
+            percentageAvailable = false;
+            temperatureAvailable = false;
+            chargingStateAvailable = false;
+        }
+    }
+
+    //Borrowed and adapted from https://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
+    //Modified to execute powershell commands instead
+    private static String execPowershell(String cmd) throws IOException {
+        String result = "";
+        InputStream inputStream = Runtime.getRuntime().exec("powershell -c " + cmd).getInputStream();
+        Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+        result = s.hasNext() ? s.next() : "";
+        s.close();
+        inputStream.close();
+        return result.trim();
+    }
+
+}
---
+++ src/mel/battchargecontroller/tests/BatteryInfoTest.java
@@ -0,0 +1,15 @@
+package mel.battchargecontroller.tests;
+
+import mel.battchargecontroller.BatteryInfo;
+
+public class BatteryInfoTest {
+    public static void main(String[] args) {
+        BatteryInfo.update();
+        System.out.printf("Battery percentage avail:\t%b\n", BatteryInfo.isPercentageAvailable());
+        System.out.printf("Battery percentage:\t\t%d\n", BatteryInfo.getPercentage());
+        System.out.printf("Battery temperature avail:\t\t%b\n", BatteryInfo.isTemperatureAvailable());
+        System.out.printf("Battery temperature:\t\t%d\n", BatteryInfo.getTemperature());
+        System.out.printf("Battery charge state avail:\t\t%b\n", BatteryInfo.isChargingStateAvailable());
+        System.out.printf("Battery charge state:\t\t%s\n", BatteryInfo.isCharging());
+    }
+}
